46a151ff950909f024e963569932abc01541f964
Carlos Eduardo <kadujunior567@gmail.com>
2025-10-04 15:45:12 -0400

    Implementações realizadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia(numero: "12344321", modelo: "Nokia 1100", imei: "123321", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone(numero: "454545", modelo: "Iphone 500", imei: "909098", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
